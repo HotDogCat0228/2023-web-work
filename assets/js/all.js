0425f97dbb4a3046b03dfce5b1c93030a5defed4
HotDogCat <g8xk7su3@gmail.com>
2023-06-01 00:19:20 +0800

    Update 2023-05-31T16:19:18.431Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,14 +1,107 @@
 "use strict";
 
-axios.get('https://2023-engineer-camp.zeabur.app/api/v1/works').then(function (response) {
-  console.log(response);
-}); //SWIPER
-
-var swiper = new Swiper(".mySwiper", {
+// Swiper in RWD
+var swiper = new Swiper("#mySwiper", {
   pagination: {
     el: ".swiper-pagination"
   }
-}); //jQuery back to top
+}); //get
+
+var currentURL = window.location.href;
+var pageName = currentURL.substring(currentURL.lastIndexOf("/") + 1);
+
+if (pageName == "index.html" || pageName == "index.html#") {
+  var el = document.querySelector('.footer-bottom');
+  el.innerHTML = '<p class="mr-100 order-1">AI工具王 © 2023</p>' + '<div class="d-flex order-2">' + '<a class="mr-20 href-hover" href="https://www.facebook.com/">Facebook</a>' + '<a class="mr-20 href-hover" href="https://twitter.com/?lang=zh-Hant">Twitter</a>' + '<a class="href-hover" href="https://www.instagram.com/">Instagram</a>' + '</div>' + '<p class="color-text order-3 href-hover" id="scrollToTop">Back to top</p>';
+}
+
+console.log(pageName);
+var filterData = [{
+  className: "filter-all",
+  text: "所有類型"
+}, {
+  className: "filter-1",
+  text: "篩選項目 1"
+}, {
+  className: "filter-2",
+  text: "篩選項目 2"
+}, {
+  className: "filter-3",
+  text: "篩選項目 3"
+}, {
+  className: "filter-4",
+  text: "篩選項目 4"
+}, {
+  className: "filter-5",
+  text: "篩選項目 5"
+}, {
+  className: "filter-6",
+  text: "篩選項目 6"
+}];
+var moduleData = [{
+  className: "module-all",
+  text: "所有模型"
+}, {
+  className: "module-1",
+  text: "卡卡"
+}, {
+  className: "module-2",
+  text: "杰杰"
+}, {
+  className: "module-3",
+  text: "琪琪"
+}, {
+  className: "module-4",
+  text: "昊昊"
+}]; //jQuery dropdown
+// 開起選單
+
+$('#filter-btn').on('click', function (e) {
+  $('#filter-menu').toggleClass('show');
+});
+$('#arrangement-btn').on('click', function (e) {
+  $('#arrangement-menu').toggleClass('show');
+});
+
+for (var i = 0; i < filterData.length; i++) {
+  $(".".concat(filterData[i].className)).on('click', function (e) {
+    e.preventDefault();
+    $('#filter-btnText').text($(this).text());
+    $('#filter-menu').classList.remove('show');
+  });
+}
+
+for (var _i = 0; _i < moduleData.length; _i++) {
+  $(".".concat(moduleData[_i].className)).on('click', function (e) {
+    e.preventDefault();
+    $('#filter-btnText').text($(this).text());
+    $('#filter-menu').classList.remove('show');
+  });
+}
+
+$('.old-to-new').on('click', function (e) {
+  e.preventDefault();
+  $('#arrangement-btnText').text($(this).text());
+  $('#arrangement-menu').classList.remove('show');
+});
+$('.new-to-old').on('click', function (e) {
+  e.preventDefault();
+  $('#arrangement-btnText').text($(this).text());
+  $('#arrangement-menu').classList.remove('show');
+}); // var cards = document.querySelectorAll('.partner-card');
+// // 找到最大的卡片高度(Swiper使用時高度會跑掉)
+// var maxHeight = 0;
+// cards.forEach(function(card) {
+//   var height = card.offsetHeight;
+//   if (height > maxHeight) {
+//     maxHeight = height;
+//   }
+// });
+// // 所有卡片元素的高度最大高度
+// cards.forEach(function(card) {
+//   card.style.height = maxHeight + 'px';
+// });
+//jQuery back to top
 
 $(function () {
   $('#scrollToTop').on("click", function () {
@@ -16,7 +109,7 @@ $(function () {
       scrollTop: 0
     }, 500);
   });
-}); // content-4 FAQ accordion
+}); // FAQ accordion
 
 var faqs = document.querySelectorAll('.faq');
 faqs.forEach(function (faq) {
