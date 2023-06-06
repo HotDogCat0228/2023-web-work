1f4d636cca03d7f34cecf5f1c5f8b38876bd8625
HotDogCat <g8xk7su3@gmail.com>
2023-06-07 00:08:24 +0800

    Update 2023-06-06T16:08:22.671Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -123,5 +123,157 @@ faqs.forEach(function (faq) {
   faq.addEventListener("click", function () {
     faq.classList.toggle("active");
   });
+}); // API串接 
+// 1.宣告變數
+
+var apiPath = 'https://2023-engineer-camp.zeabur.app';
+var list = document.querySelector('#list'); //抓取DOM
+
+var pagination = document.querySelector('#pagination');
+var data = {
+  type: '',
+  sort: 0,
+  page: 1,
+  search: ''
+};
+var worksData = [];
+var pagesData = {}; // 2.定義工具函式
+
+function getData(_ref) {
+  var type = _ref.type,
+      sort = _ref.sort,
+      page = _ref.page,
+      search = _ref.search;
+  var apiUrl = "".concat(apiPath, "/api/v1/works?sort=").concat(sort, "&page=").concat(page, "&").concat(type ? "type=".concat(type, "&") : '').concat(search ? "search=".concat(search) : '');
+  axios.get(apiUrl).then(function (res) {
+    worksData = res.data.ai_works.data;
+    pagesData = res.data.ai_works.page;
+    renderWorks();
+    renderPages();
+  });
+}
+
+getData(data); //3.將作品渲染至業面
+
+function renderWorks() {
+  var works = '';
+  worksData.forEach(function (item) {
+    works +=
+    /*html*/
+    "<li class=\"AI-card\">\n    <div class=\"img-list\">\n      <img src=\"".concat(item.imageUrl, "\" alt=\"ai image\">\n    </div>            \n      <div class=\"card-padding flex-grow-1\">\n      <h3 class=\"fw-900 fz-20 mb-12\">").concat(item.title, "</h3>\n      <p class=\"fz-14 font-second\">").concat(item.description, "</p>\n      </div>          \n      <div class=\"d-flex justify-content-between card-padding border-top\">\n          <h3 class=\"fw-700\">AI \u6A21\u578B</h3>\n          <p class=\"font-primary\">").concat(item.model, "</p>\n      </div>\n      <div class=\"d-flex justify-content-between card-padding border-top\">\n          <p class=\"font-primary\">#").concat(item.type, "</p>\n          <p><a href=\"").concat(item.link, "\"><span class=\"material-symbols-outlined\">\n            share\n            </span></a></p>\n      </div>            \n</li>");
+  });
+  list.innerHTML = works;
+} // 切換分頁
+
+
+{
+  /* <ul class="d-flex justify-content-end">
+   <li><a class="btn btn-black btn-black-start" href="#" id="page-link">1</a></li>
+   <li><a class="btn btn-black" href="#" id="page-link">2</a></li>
+   <li><a class="btn btn-black" href="#" id="page-link">3</a></li>
+   <li><a class="btn btn-black" href="#" id="page-link">4</a></li>
+   <li><a class="btn btn-black" href="#" id="page-link">5</a></li>
+   <li><a class="btn btn-black" href="#" id="page-link"><span class="material-symbols-outlined">
+     arrow_forward_ios
+     </span></a></li>
+  </ul> */
+}
+
+function changePage(pagesData) {
+  var pageLinks = document.querySelectorAll('#page-link');
+  var pageId = '';
+  pageLinks.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+      e.preventDefault();
+      pageId = e.target.dataset.page;
+      data.page = Number(pageId);
+
+      if (!pageId) {
+        data.page = Number(pagesData.current_page) + 1;
+      }
+
+      getData(data);
+    });
+  });
+} // 分頁選染至畫面
+
+
+function renderPages() {
+  var pageStr = '';
+
+  for (var _i2 = 1; _i2 <= pagesData.total_pages; _i2 += 1) {
+    pageStr +=
+    /*html*/
+    "<li>\n      <a class=\"".concat(pagesData.current_page == _i2 ? 'disabled' : '', " btn btn-black ").concat(pagesData.current_page == _i2 ? 'active' : '', "\" href=\"#\" id=\"page-link\" data-page=\"").concat(_i2, "\">").concat(_i2, "</a>\n    </li>");
+  }
+
+  ;
+
+  if (pagesData.has_next) {
+    pageStr +=
+    /*html*/
+    "<li>\n      <a class=\"\" href=\"#\" id=\"page-link\">\n        <span class=\"material-symbols-outlined\">\n          arrow_forward_ios\n        </span>\n      </a>\n    </li>";
+  }
+
+  ;
+  pagination.innerHTML = pageStr;
+  changePage(pagesData);
+} // 切換作品排序
+
+
+var desc = document.querySelector('#desc');
+var asc = document.querySelector('#asc'); //  由新到舊 -> sort = 0
+
+desc.addEventListener('click', function (e) {
+  e.preventDefault();
+  data.sort = 0;
+  getData(data);
+}); //  由舊到新 -> sort = 1
+
+asc.addEventListener('click', function (e) {
+  e.preventDefault();
+  data.sort = 1;
+  getData(data);
+}); // 切換作品類型
+
+var filterBtns = document.querySelectorAll('#class-btn');
+var typeBtns = document.querySelectorAll('#type-btn');
+var checkBtns = document.querySelectorAll('#type-item-check');
+console.log(checkBtns);
+filterBtns.forEach(function (item, i) {
+  item.addEventListener('click', function () {
+    filterBtns.forEach(function (btn) {
+      return btn.classList.remove("btn-gray-start");
+    });
+    item.classList.add("btn-gray-start");
+
+    if (item.textContent === '全部') {
+      data.type = '';
+    } else {
+      data.type = item.textContent;
+    }
+
+    getData(data);
+  });
+});
+typeBtns.forEach(function (item, i) {
+  item.addEventListener('click', function () {
+    if (item.textContent === '全部') {
+      data.type = '';
+    } else {
+      data.type = item.textContent;
+    }
+
+    getData(data);
+  });
+}); // 搜尋
+
+var search = document.querySelector('#search');
+search.addEventListener('keydown', function (e) {
+  if (e.keyCode === 13) {
+    data.search = search.value;
+    data.page = 1;
+    getData(data);
+  }
 });
 //# sourceMappingURL=all.js.map
