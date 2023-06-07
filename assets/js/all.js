8676270642fbd5f9d9c0a3790c378342adf1b271
HotDogCat <g8xk7su3@gmail.com>
2023-06-07 18:31:37 +0800

    Update 2023-06-07T10:31:35.401Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -86,7 +86,7 @@ for (var i = 0; i < filterData.length; i++) {
   $(".".concat(filterData[i].className)).on('click', function (e) {
     e.preventDefault();
     $('#filter-btnText').text($(this).text());
-    $('#filter-menu').classList.remove('show');
+    $('#filter-menu').removeClass('show');
   });
 }
 
@@ -94,19 +94,19 @@ for (var _i = 0; _i < moduleData.length; _i++) {
   $(".".concat(moduleData[_i].className)).on('click', function (e) {
     e.preventDefault();
     $('#filter-btnText').text($(this).text());
-    $('#filter-menu').classList.remove('show');
+    $('#filter-menu').addClass('show');
   });
 }
 
 $('.old-to-new').on('click', function (e) {
   e.preventDefault();
   $('#arrangement-btnText').text($(this).text());
-  $('#arrangement-menu').classList.remove('show');
+  $('#arrangement-menu').removeClass('show');
 });
 $('.new-to-old').on('click', function (e) {
   e.preventDefault();
   $('#arrangement-btnText').text($(this).text());
-  $('#arrangement-menu').classList.remove('show');
+  $('#arrangement-menu').removeClass('show');
 }); //jQuery back to top
 
 $(function () {
@@ -137,7 +137,8 @@ var data = {
   search: ''
 };
 var worksData = [];
-var pagesData = {}; // 2.定義工具函式
+var pagesData = {};
+var dataLength = 0; // 2.定義工具函式
 
 function getData(_ref) {
   var type = _ref.type,
@@ -147,7 +148,8 @@ function getData(_ref) {
   var apiUrl = "".concat(apiPath, "/api/v1/works?sort=").concat(sort, "&page=").concat(page, "&").concat(type ? "type=".concat(type, "&") : '').concat(search ? "search=".concat(search) : '');
   axios.get(apiUrl).then(function (res) {
     worksData = res.data.ai_works.data;
-    pagesData = res.data.ai_works.page;
+    pagesData = res.data.ai_works.page; // console.log(worksData.length);資料數量
+
     renderWorks();
     renderPages();
   });
@@ -157,27 +159,22 @@ getData(data); //3.將作品渲染至業面
 
 function renderWorks() {
   var works = '';
-  worksData.forEach(function (item) {
-    works +=
-    /*html*/
-    "<li class=\"AI-card\">\n    <div class=\"img-list\">\n      <img src=\"".concat(item.imageUrl, "\" alt=\"ai image\">\n    </div>            \n      <div class=\"card-padding flex-grow-1\">\n      <h3 class=\"fw-900 fz-20 mb-12\">").concat(item.title, "</h3>\n      <p class=\"fz-14 font-second\">").concat(item.description, "</p>\n      </div>          \n      <div class=\"d-flex justify-content-between card-padding border-top\">\n          <h3 class=\"fw-700\">AI \u6A21\u578B</h3>\n          <p class=\"font-primary\">").concat(item.model, "</p>\n      </div>\n      <div class=\"d-flex justify-content-between card-padding border-top\">\n          <p class=\"font-primary\">#").concat(item.type, "</p>\n          <p><a href=\"").concat(item.link, "\"><span class=\"material-symbols-outlined\">\n            share\n            </span></a></p>\n      </div>            \n</li>");
-  });
-  list.innerHTML = works;
+
+  if (worksData.length > 0) {
+    list.classList.remove('justify-content-center');
+    worksData.forEach(function (item) {
+      works +=
+      /*html*/
+      "<li class=\"AI-card\">\n    <div class=\"img-list\">\n      <img src=\"".concat(item.imageUrl, "\" alt=\"ai image\">\n    </div>            \n      <div class=\"card-padding flex-grow-1\">\n      <h3 class=\"fw-900 fz-20 mb-12\">").concat(item.title, "</h3>\n      <p class=\"fz-14 font-second\">").concat(item.description, "</p>\n      </div>          \n      <div class=\"d-flex justify-content-between card-padding border-top\">\n          <h3 class=\"fw-700\">AI \u6A21\u578B</h3>\n          <p class=\"font-primary\">").concat(item.model, "</p>\n      </div>\n      <div class=\"d-flex justify-content-between card-padding border-top\">\n          <p class=\"font-primary\">#").concat(item.type, "</p>\n          <p><a href=\"").concat(item.link, "\"><span class=\"material-symbols-outlined\">\n            share\n            </span></a></p>\n      </div>            \n  </li>");
+    });
+    list.innerHTML = works + '<i></i><i></i><i></i><i></i><i></i>';
+  } else {
+    list.classList.add('justify-content-center');
+    works = "<div class=\"flex-colum justift-content-center align-items-center\"><img src=\"./assets/images/embarrassed.png\" alt=\"\"><p class=\"font-primary text-align-center\">\u76EE\u524D\u7121\u8CC7\u6599</p></div>";
+    list.innerHTML = works;
+  }
 } // 切換分頁
 
-
-{
-  /* <ul class="d-flex justify-content-end">
-   <li><a class="btn btn-black btn-black-start" href="#" id="page-link">1</a></li>
-   <li><a class="btn btn-black" href="#" id="page-link">2</a></li>
-   <li><a class="btn btn-black" href="#" id="page-link">3</a></li>
-   <li><a class="btn btn-black" href="#" id="page-link">4</a></li>
-   <li><a class="btn btn-black" href="#" id="page-link">5</a></li>
-   <li><a class="btn btn-black" href="#" id="page-link"><span class="material-symbols-outlined">
-     arrow_forward_ios
-     </span></a></li>
-  </ul> */
-}
 
 function changePage(pagesData) {
   var pageLinks = document.querySelectorAll('#page-link');
@@ -212,7 +209,7 @@ function renderPages() {
   if (pagesData.has_next) {
     pageStr +=
     /*html*/
-    "<li>\n      <a class=\"\" href=\"#\" id=\"page-link\">\n        <span class=\"material-symbols-outlined\">\n          arrow_forward_ios\n        </span>\n      </a>\n    </li>";
+    "<li>\n      <a class=\"\" href=\"#\" id=\"page-link\">\n        <span class=\"material-symbols-outlined\">s\n          arrow_forward_ios\n        </span>\n      </a>\n    </li>";
   }
 
   ;
@@ -239,7 +236,6 @@ asc.addEventListener('click', function (e) {
 var filterBtns = document.querySelectorAll('#class-btn');
 var typeBtns = document.querySelectorAll('#type-btn');
 var checkBtns = document.querySelectorAll('#type-item-check');
-console.log(checkBtns);
 filterBtns.forEach(function (item, i) {
   item.addEventListener('click', function () {
     filterBtns.forEach(function (btn) {
